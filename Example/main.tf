a8731a2ee2db66a1ff457ff6ed76c0b4db4a0136
srini <mareedu868@gmail.com>
2023-03-01 13:51:08 +0530

    nextops

--- Example/main.tf
+++ Example/main.tf
@@ -1,18 +1,10 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hasicorp/azurerm"
-      version = "=3.0.0"
-    }
-    aws   = {
+        aws   = {
       source  =  "hashicorp/aws"
       version = "~> 4.0"
     }
   }
-}
-
-provider "azurerm" {
-    features {}
 }
 
 provider "aws" {
